8024510b3ac37736cb97ab65002e843e69578a81
MaksChervyakov <freelancemaksimchervyako@gmail.com>
2017-07-02 22:17:20 +0300

    работа над оптимизацией

---
+++ Продвижение групп вконтакте2/Исходники/js/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".phone-js").mask("+9 (999) 999-99-99");
+	$('.bxslider').bxSlider();
+
+
+
+	$("header .col-md-6").animated("pulse", "zoomOutDown");
+	$("header form").animated("bounceInUp", "zoomOutDown");
+	$(".title-white, .title-black").animated("zoomInUp", "zoomOutDown");
+	$(".request input").animated("fadeInLeft", "zoomOutDown");
+	$(".request button").animated("fadeInRight", "zoomOutDown");
+	$(".differences-other").animated("fadeInLeft", "zoomOutDown");
+	$(".differences-we").animated("fadeInRight", "zoomOutDown");
+});
